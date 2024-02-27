69b0784510e9f0a9a4ebe50d46cbdf18e6035dba
IntelMin <intelmin0915@gmail.com>
2024-02-27 18:10:13 +0200

    made sign in modal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,73 @@
 'use client';
 
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import SearchBox from "./searchbox";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion"
+
 import logoImg from "../../public/logo.svg";
 import userImg from "../../public/user.svg";
+import closeImg from "../../public/close.svg";
+
+const variants = {
+  hidden: { opacity: 0, x: 200, y: -20 },
+  enter: { opacity: 1, x: 50, y: -20 },
+}
 
 const Header = () => {
   const pathname = usePathname();
+  const [viewsign, setViewsign] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setViewsign(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className={`px-10 fixed w-full bg-opacity-70 ${pathname === "/search" && 'bg-black'}`}>
       <div className="w-full flex px-4 py-6">
         {pathname === "/search" && (
           <div className="flex">
-            <Image width={48} height={48} src={logoImg} alt="LOGO" className="mr-8"/>
+            <Image width={48} height={48} src={logoImg} alt="LOGO" className="mr-8" />
             <SearchBox />
           </div>
         )}
-        <div className="flex rounded-full bg-slate-600 p-4 w-12 h-12 hover:bg-stone-500 hover:text-stone-700 ml-auto">
+        <div className="flex cursor-pointer rounded-full bg-stone-900 border bg-opacity-70 p-4 w-12 h-12 hover:bg-stone-500 hover:text-stone-700 ml-auto" onClick={() => setViewsign(!viewsign)}>
           <Image src={userImg} alt={userImg}></Image>
         </div>
+        {viewsign &&
+          <motion.main
+            variants={variants}
+            initial="hidden"
+            animate="enter"
+            transition={{ type: "linear" }}
+          ><div ref={ref} className="flex fade fixed ml-auto w-[300px] bg-opacity-30 rounded-2xl border border-stone-500 right-20 top-20 h-[160px] bg-stone-900">
+              <div className="w-1/4 flex border-r border-stone-500">
+                <Image width={48} height={48} src={logoImg} alt="LOGO" className="mx-auto" />
+              </div>
+              <div className="w-5/8 p-4">
+                  <p className="text-white text-sm">Sign In With Wallet</p>
+                  <p className="text-white text-sm">To ZKML AI Search</p>
+                  <p className="text-stone-500 text-sm my-3">You can see better result by sign in</p>
+                  <button className="px-2 py-1 rounded-2xl text-white text-sm" style={{backgroundColor : "#05B7D1"}}>Connect Wallet</button>
+              </div>
+              <div className="w-1/8 p-2">
+                <Image width={36} height={36} src={closeImg} alt="close" className="ml-auto cursor-pointer rounded-full" onClick={() => setViewsign(false)}/>
+              </div>
+            </div>
+          </motion.main>
+        }
       </div>
     </div>
   );
