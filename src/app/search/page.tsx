54686dfd8591a93308d4a2f30112fba6c06b7726
IntelMin <intelmin0915@gmail.com>
2024-03-02 06:00:03 +0900

    fixed size of images and videos

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -12,7 +12,7 @@ import { AxonDataEntry, DataEntry } from "@/data/corceltypes";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Page() {
   const { address, isConnected } = useAccount();
@@ -27,35 +27,33 @@ export default function Page() {
   async function fetchCorcelText() {
     try {
       const response = await fetch("/api/corcel", {
-        method: 'POST',
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ query: q }),
       });
 
-
       if (!response.ok) {
-        throw new Error('Failed to fetch data from server');
+        throw new Error("Failed to fetch data from server");
       }
 
       const responseData = await response.json();
 
       const { data } = responseData;
-      setSummary(data)
+      setSummary(data);
       if (!data || !Array.isArray(data)) {
-        throw new Error('Invalid data format received from server');
+        throw new Error("Invalid data format received from server");
       }
-
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   }
 
   async function fetchDuckData() {
     try {
       const response = await fetch("/api/duck", {
-        method: 'POST',
+        method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
@@ -63,20 +61,19 @@ export default function Page() {
       });
 
       if (!response.ok) {
-        throw new Error('Failed to fetch data from server');
+        throw new Error("Failed to fetch data from server");
       }
 
       const responseData = await response.json();
 
       const { data } = responseData;
 
-      setResult(data)
+      setResult(data);
       if (!data || !Array.isArray(data)) {
-        throw new Error('Invalid data format received from server');
+        throw new Error("Invalid data format received from server");
       }
-
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   }
 
@@ -87,57 +84,69 @@ export default function Page() {
       await fetchCorcelText();
       setLoading(false);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
-  }
+  };
 
   useEffect(() => {
     fetchData();
-  }, [q])
+  }, [q]);
 
   useEffect(() => {
     if (!address) {
       router.push("/");
     }
-  }, [])
+  }, []);
 
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="bottom-0 w-full flex justify-center mt-28 flex-col xl:flex-row">
-          <div className="flex-auto w-full xl:w-3/5" style={{ marginBottom: "100px" }}>
+          <div
+            className="flex-auto w-full xl:w-3/5"
+            style={{ marginBottom: "100px" }}
+          >
             <div className="content-group-div mx-12 rounded-2xl p-4 content-group-left xl:ml-12 xl:mr-4">
-              {loading ? (<div className="content-div rounded-2xl mb-4 p-4 h-[32vh] flex flex-col gap-4">
-                <Skeleton className="w-32 h-7 rounded-full" />
-                <Skeleton className="w-[40vw] h-5 rounded-full" />
-                <Skeleton className="w-[42vw] h-5 rounded-full" />
-                <Skeleton className="w-[36vw] h-5 rounded-full" />
-                <Skeleton className="w-[30vw] h-5 rounded-full" />
-              </div>
-              ) : (<>{
-                summary && summary.length > 0 && (
-                  <Summary description={summary[0].choices[0].delta.content} />
-                )}</>
+              {loading ? (
+                <div className="content-div rounded-2xl mb-4 p-4 h-[32vh] flex flex-col gap-4">
+                  <Skeleton className="w-32 h-7 rounded-full" />
+                  <Skeleton className="w-[40vw] h-5 rounded-full" />
+                  <Skeleton className="w-[42vw] h-5 rounded-full" />
+                  <Skeleton className="w-[36vw] h-5 rounded-full" />
+                  <Skeleton className="w-[30vw] h-5 rounded-full" />
+                </div>
+              ) : (
+                <>
+                  {summary && summary.length > 0 && (
+                    <Summary
+                      description={summary[0].choices[0].delta.content}
+                    />
+                  )}
+                </>
               )}
-              {loading ? (<div className="content-div rounded-2xl mb-4 p-4 flex flex-col gap-8">
-                <Skeleton className="w-32 h-7 rounded-full" />
-                <div className="flex flex-col gap-6">
-                  <Skeleton className="w-[38vw] h-5 rounded-full" />
-                  <div className="flex flex-col gap-4">
-                    <Skeleton className="w-[45vw] h-5 rounded-full" />
-                    <Skeleton className="w-[48vw] h-5 rounded-full" />
+              {loading ? (
+                <div className="content-div rounded-2xl mb-4 p-4 flex flex-col gap-8">
+                  <Skeleton className="w-32 h-7 rounded-full" />
+                  <div className="flex flex-col gap-6">
+                    <Skeleton className="w-[38vw] h-5 rounded-full" />
+                    <div className="flex flex-col gap-4">
+                      <Skeleton className="w-[45vw] h-5 rounded-full" />
+                      <Skeleton className="w-[48vw] h-5 rounded-full" />
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-6">
+                    <Skeleton className="w-[38vw] h-5 rounded-full" />
+                    <div className="flex flex-col gap-4">
+                      <Skeleton className="w-[45vw] h-5 rounded-full" />
+                      <Skeleton className="w-[48vw] h-5 rounded-full" />
+                    </div>
                   </div>
                 </div>
-                <div className="flex flex-col gap-6">
-                  <Skeleton className="w-[38vw] h-5 rounded-full" />
-                  <div className="flex flex-col gap-4">
-                    <Skeleton className="w-[45vw] h-5 rounded-full" />
-                    <Skeleton className="w-[48vw] h-5 rounded-full" />
-                  </div>
-                </div>
-              </div>) : (<>
-                {result && <RelevantLinks links={result.organic_results} />}
-              </>)}
+              ) : (
+                <>
+                  {result && <RelevantLinks links={result.organic_results} />}
+                </>
+              )}
             </div>
           </div>
 
@@ -146,12 +155,14 @@ export default function Page() {
               <div className="content-group-div mx-12 xl:ml-4 xl:mr-12 mb-4 rounded-2xl p-4 content-group-right-first content-group-right1 overflow-hidden ">
                 <p className="text-white text-lg mb-3">Image</p>
                 <div className="content-group-video">
-                  <ScrollArea className="mb-3">
-                    {loading ? (<div className="flex flex-row justify-around">
+                  {loading ? (
+                    <div className="flex flex-row justify-around">
                       <Skeleton className="w-[10vw] h-[15vh]" />
                       <Skeleton className="w-[10vw] h-[15vh]" />
                       <Skeleton className="w-[10vw] h-[15vh]" />
-                    </div>) : (<>
+                    </div>
+                  ) : (
+                    <>
                       {result.inline_images.map((image, index) => (
                         <ImageCard
                           key={index}
@@ -160,8 +171,8 @@ export default function Page() {
                           url={image.link}
                         />
                       ))}
-                    </>)}
-                  </ScrollArea>
+                    </>
+                  )}
                 </div>
               </div>
             )}
@@ -169,12 +180,14 @@ export default function Page() {
               <div className="content-group-div mx-12 xl:ml-4 xl:mr-12 mb-4 rounded-2xl p-4 content-group-right-first content-group-right1 overflow-hidden ">
                 <p className="text-white text-lg mb-3">Video</p>
                 <div className="content-group-video">
-                  <ScrollArea className="mb-3">
-                    {loading ? (<div className="flex flex-row justify-around">
+                  {loading ? (
+                    <div className="flex flex-row justify-around">
                       <Skeleton className="w-[10vw] h-[15vh]" />
                       <Skeleton className="w-[10vw] h-[15vh]" />
                       <Skeleton className="w-[10vw] h-[15vh]" />
-                    </div>) : (<>
+                    </div>
+                  ) : (
+                    <>
                       {result &&
                         result.inline_videos &&
                         result.inline_videos.map((video, index) => (
@@ -186,28 +199,32 @@ export default function Page() {
                             duration={video.duration}
                           />
                         ))}
-                    </>)}
-                  </ScrollArea>
+                    </>
+                  )}
                 </div>
               </div>
             )}
 
             <ScrollArea className="content-group-div mx-12 xl:ml-4 xl:mr-12 rounded-2xl p-4 content-group-right-first content-group-right2 overflow-hidden h-[19vh]">
-              {loading ? (<div className="flex flex-row justify-around">
-                <Skeleton className="w-[14vw] h-[5vh]" />
-                <Skeleton className="w-[14vw] h-[5vh]" />
-              </div>) : (<>
-                {result &&
-                  result.related_searches &&
-                  result.related_searches.map((related, index) => (
-                    <RelatedLink
-                      key={index}
-                      link={related.link}
-                      query={related.query}
-                    />
-                  ))
-                }
-              </>)}
+              {loading ? (
+                <div className="flex flex-row justify-around">
+                  <Skeleton className="w-[10vw] h-[5vh]" />
+                  <Skeleton className="w-[10vw] h-[5vh]" />
+                  <Skeleton className="w-[10vw] h-[5vh]" />
+                </div>
+              ) : (
+                <>
+                  {result &&
+                    result.related_searches &&
+                    result.related_searches.map((related, index) => (
+                      <RelatedLink
+                        key={index}
+                        link={related.link}
+                        query={related.query}
+                      />
+                    ))}
+                </>
+              )}
             </ScrollArea>
           </div>
         </div>
