15dd681e6d9707182cf393e65b3e1e63010ec788
IntelMin <intelmin0915@gmail.com>
2024-02-29 22:26:05 -0800

    feat/lazy loading with skelenton

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -12,6 +12,7 @@ import { AxonDataEntry, DataEntry } from "@/data/corceltypes";
 import { useAccount } from "wagmi";
 import { useRouter } from "next/navigation";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function Page() {
   const { address, isConnected } = useAccount();
@@ -32,6 +33,7 @@ export default function Page() {
         },
         body: JSON.stringify({ query: q }),
       });
+
 
       if (!response.ok) {
         throw new Error('Failed to fetch data from server');
@@ -78,11 +80,19 @@ export default function Page() {
     }
   }
 
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      await fetchDuckData();
+      await fetchCorcelText();
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
+
   useEffect(() => {
-    setLoading(true);
-    fetchDuckData();
-    fetchCorcelText();
-    setLoading(false);
+    fetchData();
   }, [q])
 
   useEffect(() => {
@@ -90,16 +100,45 @@ export default function Page() {
       router.push("/");
     }
   }, [])
+
   return (
     <>
       <div className="flex flex-col items-center">
         <div className="bottom-0 w-full flex justify-center mt-28 flex-col xl:flex-row">
           <div className="flex-auto w-full xl:w-3/5" style={{ marginBottom: "100px" }}>
             <div className="content-group-div mx-12 rounded-2xl p-4 content-group-left xl:ml-12 xl:mr-4">
-              {summary ? summary.length > 0 && (
-                <Summary description={summary[0].choices[0].delta.content} />
-              ) : <div className="text-white text-2xl text-center">loading...</div>}
-              {result && <RelevantLinks links={result.organic_results} />}
+              {loading ? (<div className="content-div rounded-2xl mb-4 p-4 h-[32vh] flex flex-col gap-4">
+                <Skeleton className="w-32 h-7 rounded-full" />
+                <Skeleton className="w-[40vw] h-5 rounded-full" />
+                <Skeleton className="w-[42vw] h-5 rounded-full" />
+                <Skeleton className="w-[36vw] h-5 rounded-full" />
+                <Skeleton className="w-[30vw] h-5 rounded-full" />
+              </div>
+              ) : (<>{
+                summary && summary.length > 0 && (
+                  <Summary description={summary[0].choices[0].delta.content} />
+                )}</>
+              )}
+              {/* {result && <RelevantLinks links={result.organic_results} />} */}
+              {loading ? (<div className="content-div rounded-2xl mb-4 p-4 h-[37vh] flex flex-col gap-8">
+                <Skeleton className="w-32 h-7 rounded-full" />
+                <div className="flex flex-col gap-6">
+                  <Skeleton className="w-[38vw] h-5 rounded-full" />
+                  <div className="flex flex-col gap-4">
+                    <Skeleton className="w-[45vw] h-5 rounded-full" />
+                    <Skeleton className="w-[48vw] h-5 rounded-full" />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-6">
+                  <Skeleton className="w-[38vw] h-5 rounded-full" />
+                  <div className="flex flex-col gap-4">
+                    <Skeleton className="w-[45vw] h-5 rounded-full" />
+                    <Skeleton className="w-[48vw] h-5 rounded-full" />
+                  </div>
+                </div>
+              </div>) : (<>
+                {result && <RelevantLinks links={result.organic_results} />}
+              </>)}
             </div>
           </div>
 
@@ -109,14 +148,20 @@ export default function Page() {
                 <p className="text-white text-lg mb-3">Image</p>
                 <div className="content-group-video">
                   <ScrollArea className="mb-3 h-[24vh]">
-                    {result.inline_images.map((image, index) => (
-                      <ImageCard
-                        key={index}
-                        imageUrl={image.thumbnail}
-                        title={image.title}
-                        url={image.link}
-                      />
-                    ))}
+                    {loading ? (<div className="flex flex-row justify-around">
+                      <Skeleton className="w-[10vw] h-[15vh]" />
+                      <Skeleton className="w-[10vw] h-[15vh]" />
+                      <Skeleton className="w-[10vw] h-[15vh]" />
+                    </div>) : (<>
+                      {result.inline_images.map((image, index) => (
+                        <ImageCard
+                          key={index}
+                          imageUrl={image.thumbnail}
+                          title={image.title}
+                          url={image.link}
+                        />
+                      ))}
+                    </>)}
                   </ScrollArea>
                 </div>
               </div>
@@ -126,32 +171,44 @@ export default function Page() {
                 <p className="text-white text-lg mb-3">Video</p>
                 <div className="content-group-video">
                   <ScrollArea className="mb-3 h-[24vh]">
-                    {result &&
-                      result.inline_videos &&
-                      result.inline_videos.map((video, index) => (
-                        <VideoCard
-                          key={index}
-                          imageUrl={video.thumbnail}
-                          title={video.title}
-                          url={video.link}
-                          duration={video.duration}
-                        />
-                      ))}
+                    {loading ? (<div className="flex flex-row justify-around">
+                      <Skeleton className="w-[10vw] h-[15vh]" />
+                      <Skeleton className="w-[10vw] h-[15vh]" />
+                      <Skeleton className="w-[10vw] h-[15vh]" />
+                    </div>) : (<>
+                      {result &&
+                        result.inline_videos &&
+                        result.inline_videos.map((video, index) => (
+                          <VideoCard
+                            key={index}
+                            imageUrl={video.thumbnail}
+                            title={video.title}
+                            url={video.link}
+                            duration={video.duration}
+                          />
+                        ))}
+                    </>)}
                   </ScrollArea>
                 </div>
               </div>
             )}
 
             <ScrollArea className="content-group-div mx-12 xl:ml-4 xl:mr-12 rounded-2xl p-4 content-group-right-first content-group-right2 overflow-hidden h-[19vh]">
-              {result ?
-                result.related_searches &&
-                result.related_searches.map((related, index) => (
-                  <RelatedLink
-                    key={index}
-                    link={related.link}
-                    query={related.query}
-                  />
-                )): <div className="text-white text-2xl text-center">loading...</div>}
+              {loading ? (<div className="flex flex-row justify-around">
+                <Skeleton className="w-[14vw] h-[5vh]" />
+                <Skeleton className="w-[14vw] h-[5vh]" />
+              </div>) : (<>
+                {result &&
+                  result.related_searches &&
+                  result.related_searches.map((related, index) => (
+                    <RelatedLink
+                      key={index}
+                      link={related.link}
+                      query={related.query}
+                    />
+                  ))
+                }
+              </>)}
             </ScrollArea>
           </div>
         </div>
