1dadd9724988cfdd8701106bea9766af105b7085
IntelMin <intelmin0915@gmail.com>
2024-03-05 03:55:40 +0900

    resolve conflicts

--- src/components/relevantlinks.tsx
+++ src/components/relevantlinks.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 const RelevantLinks: React.FC<{ links: OrganicResult[] }> = ({ links }) => {
   return (
-    <ScrollArea className="content-div rounded-xl mb-4 p-4">
+    <ScrollArea className="content-div rounded-2xl mb-4 p-4">
       <p className="text-white text-lg mb-3">Relevant Links:</p>
 
       <div className="relevant-links">
@@ -14,14 +14,20 @@ const RelevantLinks: React.FC<{ links: OrganicResult[] }> = ({ links }) => {
           <div key={link.link} className="relevant-link">
             {" "}
             {/* Use link.link as the key */}
-            <p className="text-base underline description-color">
+            <p className="text-base description-color ml-2">
+              {link.pagemap?.cse_thumbnail && <img
+                className="inline-block mr-2 rounded-lg"
+                src={link.pagemap.cse_thumbnail[0]?.src}
+                alt={link.title}
+                style={{ width: "40px", height: "40px" }}
+              />}
               {index + 1}.{" "}
-              <a href={link.link} target="_blank" rel="noopener noreferrer">
+              <a href={link.link} target="_blank" rel="noopener noreferrer" className="underline">
                 {link.title}
               </a>
             </p>
             <br />
-            <p className="text-base description-color">{link.snippet}</p>{" "}
+            <p className="text-base description-color ml-4">{link.snippet}</p>{" "}
             {/* Use link.snippet */}
             <br />
           </div>
